b61e816271b8395f19a1ccf356a1e91abb2a1cd8
Eightaan <57832730+Eightaan@users.noreply.github.com>
2022-06-22 00:54:20 +0200

    Add files via upload

--- mods/Hotline Miami Combo Counter/lua/PlayerManager.lua
+++ mods/Hotline Miami Combo Counter/lua/PlayerManager.lua
@@ -1,7 +1,5 @@
 Hooks:PostHook(PlayerManager, "on_killshot", "combo_update", function(self, killed_unit)
-	if not CopDamage.is_civilian(killed_unit:base()._tweak_table) then
-	  HotlineHUD.Kills = HotlineHUD.Kills + 1
-	  HotlineHUD.LastKillTime = HotlineHUD.KillTime
-	  HotlineHUD.KillTime = managers.hud._current_time
+    if not CopDamage.is_civilian(killed_unit:base()._tweak_table) then
+        managers.hud:HMHCC_OnKillshot()
     end
 end)
